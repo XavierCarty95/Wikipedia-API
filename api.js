b3919702946656511dcb57a19af3cc2501c71165
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-02 03:20:34 +0000

    forEach displaying in index.html

--- api.js
+++ api.js
@@ -1,50 +1,25 @@
-
-// Load Daily Bitcoin Data
-function loadWiki() {
+function myApp(){
+    const button = document.getElementById("button")
+    button.addEventListener("click" , myButton)
     
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.open('GET', "https://en.wikipedia.org/w/api.php", true);
-    
-    xhttp.onload = function() {
-        if (this.status == 200) {
-            let wikiData = JSON.parse(this.responseText);
-            document.getElementById("Display").innerHTML = wikiData
-            
-            var data = document.getElementById("wikiSearchData").value
-            var output = "<ul>"
-            for (var i = 0; i < wikiData.length ; i++){
-              
-              output += "<li>" + data + "<li>"
-            }
-            
-            output += "<ul>"
-            FetchData()
-            
-            
-        }    
-    };
-xhttp.send();        
-}   
-
-
-document.getElementById('wikiSearch').addEventListener('click', loadWiki);
-
-function FetchData(){
-  
-
-fetch('"https://en.wikipedia.org/w/api.php')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
 }
 
 
-function handleSubmit(e){
-  e.preventDefault()
+
+function myButton(){
+    console.log("The button is clicked")
+    document.getElementById("myText").innerHTML = "This button is click"
+    var array = ["Xavier" , "Kyle" , "rick " , "david"]
+    var list = ""
+      array.forEach(function(item){
+          const listNode = "<li> " + item +  "</li>"
+          
+          list += listNode;
+          
+          console.log(item)
+      })
+    
+    document.getElementById("list").innerHTML = list 
 }
 
 
@@ -53,29 +28,37 @@ function handleSubmit(e){
 
 
 
+// var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=''";
 
 
-// $(document).ready(function() {
 
-//     let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&formatversion=2&search=";
+
+// function sumbit(e){
+//     e.preventDefault()
+// }
+
+// function displayData(data){ 
+//     return data
+    
    
-//     $("#random").click(function() {
-//         window.open('https://en.wikipedia.org/wiki/Special:Random');
-//     });
+    
+// }
 
-//     $("#btn").click(function() {
-//       $("#content").empty();
-//       let term = $("#search-text").val();
-//       let url = wikiUrl + term;
- 
-//       $.getJSON(url, function(json){
-//           $("#content").html("<h2 class='text-center font-style result-title' style='margin-top: 10px;'>Search Results for: '" + json[0] + "'");
-//           for (let i = 0; i < json[1].length; i++) {    
-//                 $("#content").append("<a id='l" + i +"' href=''><div id='c"+ i +"' class='results items font-style'><p class='font-style results-title'>" + json[1][i] + "</p></div></a>");
-//                 $(`#c${i}`).append("<p class='results-content'>" + json[2][i] + "</p>");
-//                 $(`#l${i}`).attr("href", json[3][i]);  
-//             }
-//         });
-//         $("#search-text").val('');
-//   });  
-// });
+
+
+
+// function getData(){ 
+    
+//   fetch(url).then(function(response){
+//     return response.json();
+// }).then(function(myJson){
+//   console.log(JSON.stringify(myJson))
+//  var data =  displayData(myJson)
+
+   
+   
+       
+// })
+
+
+}
